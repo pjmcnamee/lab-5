a104a12cfa566fb5b04c2fe01ddcd267b3f7496e
Patrick McNamee <pjmcnamee@comcast.net>
2018-08-10 15:07:43 -0700

    Question three done

--- app.js
+++ app.js
@@ -52,11 +52,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+    var output = sum(sum(a,b)[0], c)[0];
+    var outputMultiply = multiply(multiply(a,b)[0], c)[0];
+    var sumMessage = + a + ' and ' + b + ' and ' + c + ' sum to ' + output + '.';
+    var multiMessage = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + outputMultiply + '.';
+    return [output, outputMultiply, sumMessage, multiMessage];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -74,7 +78,7 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+    
 }
 
 // Here is the test for sumArray(); uncomment it to run it
